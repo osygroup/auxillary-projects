e683054004423e59f5305dfdce4f202147cec2ab
osygroup <46828049+osygroup@users.noreply.github.com>
2021-02-02 20:25:51 -0600

    Update bulkusers.sh

--- bulkusers.sh
+++ bulkusers.sh
@@ -1,1 +1,1 @@
-sudo awk '{print "Create user: "$1; system("sudo useradd --group testgroup -m -k <file_path_to_folder_containing_.ssh_folder> "$1)}' $1
+sudo awk '{print "Create user: "$1; system("sudo useradd --group developers -m -k <file_path_to_folder_containing_.ssh_folder> "$1)}' $1
